68632237d97f50c938fdbddd3dd461c9000a5fce
VaibhavLande84 <vaibhavlande33@gmail.com>
2026-06-26 23:18:04 +0530

    doc.cc

--- backend/models/doc.cc
+++ backend/models/doc.cc
@@ -2,12 +2,11 @@
 #include <iostream>
 #include <utility>
 
-#include "../adaptor/mongoDBadaptor.h"
+#include "../managers/storage_manager.h"
 
 void getSpecificDocument(int doc_ID, int User_ID,
                          std::function<void(const Document&)> onSuccess,
                          std::function<void(const std::string&)> onError)
 {
-    MongoDB_adaptor adaptor;
-    adaptor.fetchSpecificDocument(doc_ID, User_ID, std::move(onSuccess), std::move(onError));
+    StorageManager::getAdaptor()->fetchSpecificDocument(doc_ID, User_ID, std::move(onSuccess), std::move(onError));
 }
